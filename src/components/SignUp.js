25db5e9d1f6449e638a6dcb2ab3a5bbda45fdaa7
vardan1218 <vardanwadhwa72@gmail.com>
2023-08-18 15:02:52 -0800

    1st question done

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './button'
+
+const SignUp = () => {
+  return (
+    <div>
+        <input type='text' placeholder='First Name' />
+        <input type='text' placeholder='Last Name' />
+        <Button />
+    </div>
+  )
+}
+
+export default SignUp
